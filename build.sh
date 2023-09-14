eea4953555c0e219bb31cc8d68a9372c4e8f98fc
Tyler K <tyly04@gmail.com>
2023-09-13 20:04:30 -0700

    Convincing image loading

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 /ucrt64/bin/meson compile -C build
 
+[ $? -ne 0 ] && read -p "Press enter to continue..."
+
 ./build/spider-880.exe
 [ $? -ne 0 ] && read -p "Press enter to exit..."
--- main.cpp
+++ main.cpp
@@ -2,18 +2,51 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_PNG_Image.H>
- 
-int main(int argc, char **argv) {
-  Fl_Window *window = new Fl_Window(340,180, "Test");
-  Fl_Box *box = new Fl_Box(20,40,300,100);
+#include <FL/fl_draw.H>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
-  Fl_PNG_Image *img = new Fl_PNG_Image("./assets/test.png");
+void draw_img(void*) {
+	static Fl_PNG_Image *img = new Fl_PNG_Image("./assets/test.png");
+	static Fl_Image* temp = img->copy(300 * img->w()/img->h(), 100 * img->w()/img->h());
 
-  Fl_Image* temp = img->copy(box->w() * img->w()/img->h(), box->h() * img->w()/img->h());
+	static int y = 0;  
+	static int x = 0;
+	static const char *buf = temp->data()[0];
 
-  box->image(temp);
-  
-  window->end();
-  window->show(argc, argv);
-  return Fl::run();
+	for (int i = 0; i < rand(); i++){
+		if (x >= temp->w()) {
+			y += 1;
+			x = 0;
+		}
+		if (y >= temp->h()) {
+			y = 0;
+			x = 0;
+			return;
+		}
+		long index = (y * temp->w() * temp->d()) + (x * temp->d());
+
+		char r = *(buf + index);
+		char g = *(buf + index + 1);
+		char b = *(buf + index + 2);
+
+		fl_color(r, g, b);
+		fl_point(x, y);
+		x += 1;
+	}
+	Fl::repeat_timeout(0.01, draw_img);
+}
+
+	int main(int argc, char **argv) {
+	Fl_Window *window = new Fl_Window(340,180, "Test");
+	Fl_Box *box = new Fl_Box(20,40,300,100);
+
+	// box->image(temp);
+
+	window->end();
+	window->show(argc, argv);
+
+	Fl::add_timeout(0.01, draw_img);
+	return Fl::run();
 }
