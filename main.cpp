ba3bdfd6c352bf4b91c3c563afebe06799b16b47
Tyler K <tyly04@gmail.com>
2023-09-13 18:34:37 -0700

    Better image display

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char **argv) {
   Fl_Window *window = new Fl_Window(340,180, "Test");
   Fl_Box *box = new Fl_Box(20,40,300,100);
 
-  Fl_PNG_Image *img = new Fl_PNG_Image("./test.png");
+  Fl_PNG_Image *img = new Fl_PNG_Image("./assets/test.png");
 
-  Fl_Image* temp = img->copy(box->w(), box->h() * img->h()/img->w());
+  Fl_Image* temp = img->copy(box->w() * img->w()/img->h(), box->h() * img->w()/img->h());
 
   box->image(temp);
   
