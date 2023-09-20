c73f5c3a258659ab7e146cbc280b212d9ca5ea23
Tyler K <tyly04@gmail.com>
2023-09-20 15:48:10 -0700

    Fixing color palette issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,17 @@
 #include <FL/Fl.H>
-#include <murder_blog/pages.h>
+#include <FL/Fl_Window.H>
+#include "util/image_box.h"
 
 #include <iostream>
 int main(int argc, char **argv) {
-	MurderBlogIndexHTMLWindow* page = new MurderBlogIndexHTMLWindow(0, 0, 300, 300);
-	page->show();
+	// MurderBlogIndexHTMLWindow* page = new MurderBlogIndexHTMLWindow(0, 0, 300, 300);
+	// page->show();
+	Fl_Window* w = new Fl_Window(0, 0, 300, 300, "test");
+	ImageBox* box = new ImageBox("./assets/palette.png", 0, 0, 300, 300);
+	w->end();
+	w->show();
 
-
-	// return Fl::run();
 	Fl::run();
 	
 	return 0;
 }
-
-/*
-
-Fl_Text_Display *p_8 = new Fl_Text_Display(0, 0, 300, 300);
-
-		// p_8->box(FL_NO_BOX);
-		// p_8->wrap_mode(3, 0);
-		Fl_Text_Buffer *p_8_text = new Fl_Text_Buffer();
-		Fl_Text_Buffer *style_buffer = new Fl_Text_Buffer();
-		Fl_Text_Display::Style_Table_Entry style_table[] = { 
-			{  FL_RED,         FL_COURIER, 18 }, // A - Red
-			{  FL_DARK_YELLOW, FL_COURIER, 18 },
-		 };
-		p_8->buffer(p_8_text);
-
-		int num = sizeof(style_table)/sizeof(style_table[0]);
-		p_8->highlight_data(style_buffer, style_table, num, 'A', 0, 0);
-
-		p_8_text->text("Test");
-		style_buffer->text("AABB");
-*/
--- src/util/color.cpp
+++ src/util/color.cpp
@@ -2,9 +2,8 @@
 #include <math.h>
 #include <FL/Fl.H>
 
-const Fl_Color PALETTE[16] = {FL_BLACK, FL_RED, FL_GREEN, FL_YELLOW, FL_BLUE, FL_MAGENTA, FL_CYAN, FL_WHITE, FL_DARK_RED, FL_DARK_GREEN, FL_DARK_YELLOW, FL_DARK_BLUE, FL_DARK_MAGENTA, FL_DARK_CYAN, FL_GRAY0, FL_DARK1}; 
+const Fl_Color PALETTE[16] = {FL_BLACK, FL_RED, FL_GREEN, FL_YELLOW, FL_BLUE, FL_MAGENTA, FL_CYAN, FL_WHITE, FL_DARK_RED, FL_DARK_GREEN, FL_DARK_YELLOW, FL_DARK_BLUE, FL_DARK_MAGENTA, FL_DARK_CYAN, FL_DARK3, FL_DARK1}; 
 
-#include <stdio.h>
 Fl_Color rgb_to_palette(unsigned char r, unsigned char g, unsigned char b) {
 	int len = sizeof(PALETTE)/sizeof(Fl_Color);
 
@@ -18,9 +17,9 @@ Fl_Color rgb_to_palette(unsigned char r, unsigned char g, unsigned char b) {
 
 		Fl::get_color(color, c_r, c_g, c_b);
 
-		double r_dist = pow(r - (unsigned int) c_r, 2);
-		double g_dist = pow(g - (unsigned int) c_g, 2);
-		double b_dist = pow(b - (unsigned int) c_b, 2);
+		double r_dist = pow(r - c_r, 2);
+		double g_dist = pow(g - c_g, 2);
+		double b_dist = pow(b - c_b, 2);
 
 		double dist = sqrt(r_dist + g_dist + b_dist);
 
