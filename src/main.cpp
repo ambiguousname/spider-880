3a7db0a0949e24e8f947e56f32fd61be8c69c7d6
Tyler K <tyly04@gmail.com>
2023-09-20 15:49:25 -0700

    Stylistic looking bug

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 	// MurderBlogIndexHTMLWindow* page = new MurderBlogIndexHTMLWindow(0, 0, 300, 300);
 	// page->show();
 	Fl_Window* w = new Fl_Window(0, 0, 300, 300, "test");
-	ImageBox* box = new ImageBox("./assets/palette.png", 0, 0, 300, 300);
+	ImageBox* box = new ImageBox("./assets/victim.png", 0, 0, 300, 300);
 	w->end();
 	w->show();
 
