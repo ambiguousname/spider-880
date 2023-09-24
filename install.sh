b0cef127645b9bdda9fde4b267e2a32d6718be44
Tyler K <tyly04@gmail.com>
2023-09-24 16:47:17 -0700

    Added install script for quick development

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if ! command -v pip > /dev/null; then
+	if  command -v pacman > /dev/null ; then
+		pacman -S mingw-w64-ucrt-x86_64-python mingw-w64-ucrt-x86_64-python-pip
+	else
+		sudo apt-get install python3 python3-pip python3-setuptools \
+							python3-wheel ninja-build
+	fi
+fi
+
+if ! command -v meson > /dev/null; then
+	pip3 install --user meson
+
+
+	script_path=$(cygpath -u $USERPROFILE)/.local/bin
+	echo "export PATH=\$PATH:$script_path" >> ~/.profile
+	source ~/.profile
+fi
+
+if  command -v pacman > /dev/null ; then
+	pacman -S mingw-w64-ucrt-x86_64-fltk
+else
+	sudo apt-get install libfltk1.3-dev libx11-dev
+fi
+
+meson setup build
