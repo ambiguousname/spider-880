12af79aac9c979b65b9de5e949009471b1845e7c
Tyler K <tyly04@gmail.com>
2023-09-27 18:35:00 -0700

    Setting up database and configuring intellisense

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,8 +1,25 @@
 #include "database.h"
+#include <stdexcept>
+#include <format>
 
 CitizenDatabase::CitizenDatabase(const char* filename) {
 	sqlite3_open(filename, &database);
-	
+}
+
+static int query_callback(db_callback callback, int argc, char** argv, char** azColName) {
+	for (int i = 0; i < argc; i++) {
+		columns.cols[azColName[i]] = argv[i];
+	}
+	callback();
+	return 0;
+}
+
+void CitizenDatabase::query(const char *query_text, db_callback callback) {
+	char *errMessage = 0;
+	int code = sqlite3_exec(database, query_text, query_callback, &columns, &errMessage);
+	if (errMessage > 0) {
+		throw std::runtime_error(std::format("SQL Error: {}", errMessage));
+	}
 }
 
 CitizenDatabase::~CitizenDatabase() {
--- src/database/database.h
+++ src/database/database.h
@@ -1,8 +1,25 @@
 #pragma once
 #include <sqlite3.h>
 
+class SQLColumns {
+	protected:
+	virtual void update(char* key, char* value) = 0;
+};
+
+class Citizen : SQLColumns {
+	protected:
+	void update(char* key, char* value) override;
+};
+
+
+template<typename T>
+typedef void (*db_callback)(T* columns);
+
 class CitizenDatabase {
 	sqlite3 *database;
+	void query(const char* query_text, db_callback callback);
+	public:
 	CitizenDatabase(const char* filename);
+	void QueryCitizen();
 	~CitizenDatabase();
 };
