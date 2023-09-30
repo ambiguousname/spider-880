5d36ec866c4439eec2a202adcf382bd89d18babc
Tyler K <tyly04@gmail.com>
2023-09-30 15:43:56 -0700

    Added query language

--- src/database/database_window.cpp
+++ src/database/database_window.cpp
@@ -1,7 +1,35 @@
 #include "database_window.h"
 #include "util/hide_all_windows.h"
 
-DatabaseWindow::DatabaseWindow(int x, int y, int w, int h) : Fl_Window(x, y, w, h, "Citizen Database"), citizen_db(), area(0, 0, 100, 20, "Area"), income(0, 20, 100, 20, "Income"), family(0, 40, 100, 20, "Family") {
+DatabaseChoice::DatabaseChoice(int x, int y, int w, int h, database_selector first, database_selector second, database_selector third) : choice(x, y, w, h), selectors({first, second, third}) {
+	
+}
+
+// TODO: Combine with INTERSECT statements.
+std::string selectArea(int tier, int value) {
+	std::string base_selector = "SELECT * FROM households WHERE SUBSTRING(households.zip, " + std::to_string(tier + 1) + ", 1) = '" + std::to_string(value) + "'";
+	if (value == 0) {
+		base_selector += " OR LENGTH(households.zip) < " + std::to_string(tier + 1);
+	}
+	base_selector += ";";
+	return base_selector;
+}
+
+std::string selectIncome(int tier, int value) {
+	if (tier == 0) {
+		return "SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id WHERE citizens.income >" + std::to_string(value) + " GROUP BY household_id;";
+	}
+	return "SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id WHERE citizens.income > X GROUP BY household_id, or for percentile rankings: SELECT households.* FROM (SELECT household_id, PERCENT_RANK() OVER(ORDER BY income) AS Percent FROM citizens JOIN households ON households.id = citizens.household_id) rankings JOIN households ON households.id = rankings.household_id WHERE rankings.Percent > " + std::to_string(value) + " GROUP BY household_id;";
+}
+
+std::string selectFamily(int tier, int value) {
+	if (tier == 2) {
+		return "SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id GROUP BY household_id HAVING count(household_id) > " + std::to_string(value) + ";";
+	}
+	return "SELECT * FROM households WHERE family_status = " + std::to_string(value) + ";";
+}
+
+DatabaseWindow::DatabaseWindow(int x, int y, int w, int h) : Fl_Window(x, y, w, h, "Citizen Database"), citizen_db(), area(0, 0, 100, 20), income(0, 20, 100, 20), family(0, 40, 100, 20), database_display(0, 80, w, h - 80) {
 	resizable(this);
 	end();
 }
--- src/database/database_window.h
+++ src/database/database_window.h
@@ -1,15 +1,28 @@
 #pragma once
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_Table.H>
 #include <memory>
-#include <FL/Fl_Light_Button.H>
+#include <string>
 #include "database.h"
+
+typedef std::string (*database_selector)(int tier, int value);
+
+struct DatabaseChoice {
+	Fl_Choice choice;
+	database_selector selectors[3];
+	DatabaseChoice(int x, int y, int w, int h, database_selector first, database_selector second, database_selector third);
+};
 
 class DatabaseWindow : public Fl_Window {
 	std::unique_ptr<CitizenDatabase> citizen_db;
 	
-	Fl_Light_Button area;
-	Fl_Light_Button income;
-	Fl_Light_Button family;
+	// For combining: JOIN households ON households.id = citizens.household_id
+	DatabaseChoice area; // SELECT * FROM households WHERE SUBSTRING(households.zip, Y, 1) = 'X'. If X is 0, instead use: SELECT * FROM households WHERE SUBSTRING(households.zip, 1, 1) = '0' OR LENGTH(households.zip) < 3, either that or: SELECT * FROM households WHERE SUBSTRING(households.zip, 2, 1) = '0' OR LENGTH(households.zip) < 3
+	DatabaseChoice income; // SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id WHERE citizens.income > X GROUP BY household_id, or for percentile rankings: SELECT households.* FROM (SELECT household_id, PERCENT_RANK() OVER(ORDER BY income) AS Percent FROM citizens JOIN households ON households.id = citizens.household_id) rankings JOIN households ON households.id = rankings.household_id WHERE rankings.Percent > 0.5;
+	DatabaseChoice family; // SELECT * FROM households WHERE family_status = X, SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id GROUP BY household_id HAVING count(household_id) > X
+
+	Fl_Table database_display;
 
 	protected:
 	void hide();
