ef4a4a66650a8b3d3b9ec5567425da707455a06d
Tyler K <tyly04@gmail.com>
2023-09-29 20:55:17 -0700

    Painfully figuring out how limited macros are

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,5 +1,51 @@
 #include "database.h"
-#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
+column_key* SQLColumns::getValuePtr(std::string key) {
+	if (key == "id") {
+		return &id;
+	}
+	return nullptr;
+}
+
+void SQLColumns::update(char* key, char* value) {
+	column_key value_key = std::make_unique<std::string>(value);
+	std::string key_string = std::string(key);
+	column_key* value_ptr = getValuePtr(key_string);
+	value_key.release();
+	if (value_ptr == nullptr) {
+		fprintf(stderr, "SQL unidentified key %s for %s", key, typeid(this).name());
+	} else {
+		value_ptr->swap(value_key);
+	}
+}
+
+column_key* Citizen::getValuePtr(std::string key) {
+	if (key == "age") {
+		return &age;	
+	} else if (key == "first_name") {
+		return &first_name;
+	} else if (key == "last_name") {
+		return &last_name;
+	} else if (key == "income") {
+		return &income;
+	} else if (key == "household_id") {
+		return &household_id;
+	} else if (key == "spouse_id") {
+		return &spouse_id;
+	}
+	return SQLColumns::getValuePtr(key);
+}
+
+column_key* Household::getValuePtr(std::string key) {
+	if (key == "family_status") {
+		return &family_status;
+	} else if (key == "zip") {
+		return &zip;
+	}
+	return SQLColumns::getValuePtr(key);
+}
 
 CitizenDatabase::CitizenDatabase(const char* filename) {
 	sqlite3_open(filename, &database);
@@ -20,9 +66,8 @@ void CitizenDatabase::query(const char *query_text, db_callback<T> callback) {
 	char *errMessage = 0;
 	int code = sqlite3_exec(database, query_text, query_callback, callback, &errMessage);
 	if (code != SQLITE_OK) {
-		char* msg = errMessage;
-		sqlite3_free(errMessage); 
-		throw std::runtime_error(msg);
+		fprintf(stderr, "SQL query error: %s", errMessage);
+		sqlite3_free(errMessage);
 	}
 }
 
--- src/database/database.h
+++ src/database/database.h
@@ -1,15 +1,38 @@
 #pragma once
 #include <sqlite3.h>
+#include <memory>
 #include <concepts>
+// TODO: I wonder if there's some kind of C++ variant that allows for macros before compiling?
+// I think having a language that pre-compiles into C++ would be suuuuper useful for readability and editing.
+// Maybe a gcc plugin or something? https://thinkingeek.com/2015/08/16/a-simple-plugin-for-gcc-part-1/
+// This completely sucks otherwise. I certainly wish C++ had a more robust macro system (like Rust. Le sigh...)
+
+typedef std::unique_ptr<std::string> column_key; 
 
 class SQLColumns {
 	protected:
-	virtual void update(char* key, char* value) = 0;
+	// Return a pointer to the smart pointer to set its value.
+	virtual column_key* getValuePtr(std::string key);
+	void update(char* key, char* value);
+	column_key id;
 };
 
 class Citizen : SQLColumns {
 	protected:
-	void update(char* key, char* value) override;
+	column_key* getValuePtr(std::string key) override;
+	column_key age;
+	column_key first_name;
+	column_key last_name;
+	column_key income;
+	column_key household_id;
+	column_key spouse_id;
+};
+
+class Household : SQLColumns {
+	protected:
+	column_key* getValuePtr(std::string key) override;
+	column_key family_status;
+	column_key zip;
 };
 
 
