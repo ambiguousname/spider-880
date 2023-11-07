cb89310d90bcfd70f35cfa43f98f0f8b1f4be93b
Tyler K <tyly04@gmail.com>
2023-11-07 14:07:45 -0800

    Redesign income selection

--- src/database/database_window.cpp
+++ src/database/database_window.cpp
@@ -79,64 +79,54 @@ std::string selectArea(int tier, int value) {
 
 // Integer formating: 1 byte for HIGH (1-255), 1 byte for LOW (1-255), so the byte formatting is: HIGHLOW on an integer.
 
+ChoiceCategory income_percent {
+	"Income %",
+	{
+		{"Top 10%", 0b1111111101011010}, // > 0.9
+		{"10%-20%", 0b0101101001010000},
+		{"20%-30%", 0b0101000001000110},
+		{"30%-40%", 0b0100011000111100},
+		{"40%-50%", 0b0011110000110010},
+		{"50%-60%", 0b0011001000101000},
+		{"60%-70%", 0b0010100000011110},
+		{"70%-80%", 0b0001111000010100},
+		{"80%-90%", 0b0001010000001010},
+		{"Bottom 90%", 0b0000101000000000}, // < 0.1
+	}
+};
+
 ChoiceCategory income_range {
 	"Income/mo",
 	{
 		{"<$5k", 0b0000010100000000},
-		{"$10k-$5k", 0b0000101000000101},
-		{"$20k-$10k",0b0001010000001010},
+		{"$5k-$10k", 0b0000101000000101},
+		{"$10k-$20k",0b0001010000001010},
 		{">$20k", 0b1111111100010100}
 	}
 };
 
-ChoiceCategory income_percent {
-	"Income %",
+ChoiceCategory income_range_fine {
+	"Income/mo",
 	{
-		{"Bottom 90%", 0b1111111101011010}, // < 90%
-		{"90%-80%", 0b0101101001010000},
-		{"80%-70%", 0b0101000001000110},
-		{"70%-60%", 0b0100011000111100},
-		{"60%-50%", 0b0011110000110010},
-		{"50%-40%", 0b0011001000101000},
-		{"40%-30%", 0b0010100000011110},
-		{"30%-20%", 0b0001111000010100},
-		{"20%-10%", 0b0001010000001010},
-		{"Top 10%", 0b0000101000000000}, // > 10%
-	}
-};
-
-ChoiceCategory income_percent_fine {
-	"Income % (Fine Control)",
-	{
-		{"Bottom 95%", 0b1111111101011111}, // < 95%
-		{"95%-90%", 0b0101111101011010},
-		{"90%-85%", 0b0101101001010101},
-		{"85%-80%", 0b0101010101010000},
-		{"80%-75%", 0b0101000001001011},
-		{"75%-70%", 0b0100101101000110},
-		{"70%-65%", 0b0100011001000001},
-		{"65%-60%", 0b0100000100111100},
-		{"60%-55%", 0b0011110000110111},
-		{"55%-50%", 0b0011011100110010},
-		{"50%-45%", 0b0011001000101101},
-		{"45%-40%", 0b0010110100101000},
-		{"40%-35%", 0b0010100000100011},
-		{"35%-30%", 0b0010001100011110},
-		{"30%-25%", 0b0001111000011001},
-		{"25%-20%", 0b0001100100010100},
-		{"20%-15%", 0b0001010000001111},
-		{"15%-10%", 0b0000111100001010},
-		{"Top 10%", 0b0000101000000000}
+		{"<$5k", 0b0000010100000000},
+		{"$5k-$8k", 0b0000100000000101},
+		{"$8k-$10k", 0b0000101000001000},
+		{"$10k-$15k", 0b0000111100001010},
+		{"$15k-$20k",0b0001010000001111},
+		{">$20k", 0b1111111100010100}
 	}
 };
 
 std::string selectIncome(int tier, int value) {
 	int high = (value >> 8) & 0b11111111;
 	int low = value & 0b11111111;
-	if (tier == 0) {
+	if (tier >= 1) {
 		return "SELECT households.* FROM households JOIN citizens ON households.id = citizens.household_id WHERE citizens.income > " + std::to_string(low * 1000) + " AND citizens.income < " + std::to_string(high * 1000) + " GROUP BY household_id";
 	}
-	return "SELECT households.* FROM (SELECT household_id, PERCENT_RANK() OVER(ORDER BY income) AS percent FROM citizens JOIN households ON households.id = citizens.household_id) rankings JOIN households ON households.id = rankings.household_id WHERE rankings.percent > 0." + std::to_string(low) + " AND rankings.percent < 0." + std::to_string(high) + " GROUP BY household_id";
+	if (high >= 255) {
+		high = 999;
+	}
+	return "SELECT households.* FROM (SELECT household_id, PERCENT_RANK() OVER(ORDER BY income) AS percent FROM citizens JOIN households ON households.id = citizens.household_id) rankings JOIN households ON households.id = rankings.household_id WHERE rankings.percent >= 0." + std::to_string(low) + " AND rankings.percent <= 0." + std::to_string(high) + " GROUP BY household_id";
 }
 
 ChoiceCategory family_married {
@@ -188,7 +178,7 @@ std::string selectFamily(int tier, int value) {
 
 ChoiceCategory area_arr[3] = {{"Area", area_options}, {"District", area_options}, {"Subdistrict", area_options}};
 
-ChoiceCategory income_arr[3] = {income_range, income_percent, income_percent_fine};
+ChoiceCategory income_arr[3] = {income_percent, income_range, income_range_fine};
 
 ChoiceCategory family_arr[3] = {family_married, family_spouse, family_count};
 
