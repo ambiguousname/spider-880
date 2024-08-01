790821dffb83ac1209fb4c1d33a0deb5c6bac23b
Tyler K <tyly04@gmail.com>
2024-07-31 17:18:41 -0700

    Started on very basic compressor
    See TODO for more

--- src/spider_navigator/pagesbuild.py
+++ src/spider_navigator/pagesbuild.py
@@ -1,39 +1,26 @@
-from ctypes import cdll
+from ctypes import cdll, c_size_t, create_string_buffer
 from os import path, scandir
 
-cryptsab = cdll.LoadLibrary("cryptsab.dll")
+cryptsab = cdll.LoadLibrary("cryptsab/build/libcryptsab.dll")
 
 def searchDir(dir):
+	foldername, ext = path.splitext(dir.name)
+
+	files_to_compress = []
 	for entry in scandir(dir):
 		filename, extension = path.splitext(entry.name)
-		if entry.is_dir():
-			searchDir(entry)
-		elif extension == ".html":
-			entry_path = path.abspath(entry.path)
-			base, full_filename = path.split(entry_path)
-			pth = path.join(base, filename + ".cpp")
 
-			file = open(entry.path, "rb")
-			file.seek(0)
-			hashed = md5(file.read()).hexdigest()
-			file.close()
-
-			file = open(entry.path, "r")
-			global hashes
-			if pth not in hashes or hashed != hashes[pth]:
-				cpp_stream = open(pth, "w")
-				parser = HTMLCPPParser(cpp_stream, pth)
-				parser.feed(file.read())
-				parser.close()
-				cpp_stream.close()
-			
-			f = open(path.join(root, "../list.txt"), "a")
-			f.write(pth + "\n")
-			f.close()
-
-			hash_file = open(path.join(root, "../hash.txt"), "a")
-			hash_file.write(hashed + "\n")
-			hash_file.close()
+		if extension == ".html":
+			files_to_compress.append("pages/{}/{}".format(foldername, entry.name).encode("utf-8"))
+	
+	tarname = "{}.tar.z".format(foldername)
+	
+	cryptsab.tar_z_compress(tarname.encode("utf-8"), len(files_to_compress), *files_to_compress)
 
 if __name__ == "__main__":
-	searchDir(path.abspath(__file__ + "/../pages/"))
+	for pagefolder in scandir(path.abspath(__file__ + "/../pages/")):
+		filename, extension = path.splitext(pagefolder.name)
+		if not pagefolder.is_dir():
+			print(filename, " is not a folder.")
+			continue
+		searchDir(pagefolder)
