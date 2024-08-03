782cf6a120d6ba4dfaab3cbe956d9ef4d404692b
Tyler K <tyly04@gmail.com>
2024-08-03 15:41:56 -0700

    Generate archive of archives

--- src/spider_navigator/pagesbuild.py
+++ src/spider_navigator/pagesbuild.py
@@ -1,5 +1,6 @@
 from ctypes import cdll, c_size_t, create_string_buffer
-from os import path, scandir
+from os import path, scandir, remove
+from sys import argv
 
 cryptsab = cdll.LoadLibrary("cryptsab/build/libcryptsab.dll")
 
@@ -17,10 +18,20 @@ def searchDir(dir):
 	
 	cryptsab.tar_z_compress(tarname.encode("utf-8"), len(files_to_compress), *files_to_compress)
 
+	return tarname.encode('utf-8')
+
 if __name__ == "__main__":
+	if len(argv) < 2:
+		raise "Need to set out archive."
+	tar_files = []
 	for pagefolder in scandir(path.abspath(__file__ + "/../pages/")):
 		filename, extension = path.splitext(pagefolder.name)
 		if not pagefolder.is_dir():
 			print(filename, " is not a folder.")
 			continue
-		searchDir(pagefolder)
+		tar_files.append(searchDir(pagefolder))
+	
+	# cryptsab.tar_z_compress(argv[1].encode("utf-8"), len(tar_files), *tar_files)
+
+	# for file in tar_files:
+	# 	remove(file)
