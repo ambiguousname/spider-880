0cce5f6850bb7102032b4f823a8a77d91daf77a6
Tyler K <tyly04@gmail.com>
2024-12-13 12:08:51 -0800

    Fixing gl_visual bug

--- src/main.cpp
+++ src/main.cpp
@@ -108,8 +108,6 @@ int main(int argc, char **argv) {
 	SoundManager::Load("./assets/startup.wav", startup);
 	startup.play();
 
-	Fl::gl_visual(FL_RGB | FL_DEPTH | FL_OPENGL3);
-
 	Fl::run();
 
 	SoundManager::Sound close;
